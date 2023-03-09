0870fc1636f6d205c0b21d942e49bae85c917edf
Bhavna Makode <bhavnamakode20@gmail.com>
2023-03-09 17:49:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'a127df620df544ada622506449649ee5'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict({
+      id: 'face-detection',
+      name: 'face-detection',
+      version: '6dc7e46bc9124c5c8824be4822abe105',
+      type: 'visual-detector',
+    }, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box} = this.state;
+    return (
+      <div className="App">
+        <ParticlesBg type='cobweb' bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div><Logo />
+          <Rank />
+         <ImageLinkForm 
+         onInputChange={this.onInputChange} 
+         onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={box} imageUrl={imageUrl} />
+        </div>
+        : (
+          route === 'signin'
+          ? <SignIn onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange} />
+        ) 
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
